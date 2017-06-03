b7fdbf3a9d6264aac1330af5c1bc4eb3ac9221c6
Artur <artur.kudyukin@yandex.ru>
2017-06-03 22:09:58 +0300

    Update tasks.js

--- gulp/paths/tasks.js
+++ gulp/paths/tasks.js
@@ -13,6 +13,7 @@ module.exports = [
   './gulp/tasks/js.lint.js',
   './gulp/tasks/copy.image.js',
   './gulp/tasks/copy.fonts.js',
+  './gulp/tasks/sprite.js',
   './gulp/tasks/sprite.svg.js'
 ];
 
